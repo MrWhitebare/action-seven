da48b59083ce6680df1961b347614eaba3b62692
MrWhitebare <1766913528@qq.com>
2024-06-06 11:55:49 +0800

    减小程序打包后的体积

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import { ConfigProvider } from 'antd'
 import { Route, Routes } from 'react-router-dom'
 import NotFound from '@/pages/404'
 import Region from '@/pages/region'
-import WebRtcUI from './pages/webrtc'
-import Content from '@/componets/content';
-import DashBoard from '@/pages/dashboard'
+import Content from '@/componets/content'
+
 
 const Login = React.lazy(()=>import('@/pages/login'));
-const Home =React.lazy(()=>import('@/pages/home'));
+const Home = React.lazy(()=>import('@/pages/home'));
+const WebRtcUI =React.lazy(()=>import('@/pages/webrtc'));
+const Dashboard = React.lazy(()=>import('@/pages/dashboard'));
 
 function App() {
 
@@ -27,7 +28,7 @@ function App() {
           <Route path='/preview' element={<Content/>}/>
           <Route path='/region' element={<Region/>}/>
           <Route path='/webrtc' element={<WebRtcUI/>}/>
-          <Route path='/dashboard' element={<DashBoard/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
           <Route path='*' element={<NotFound/>}/>
         </Route>
       </Routes>
